e2dd6239151ac21e06463d3a21a20555835032fd
Ryuya Ishibashi <vincitori.o.morti@gmail.com>
2023-01-21 08:12:29 +0900

    add title uniqueness validation

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -10,11 +10,24 @@ RSpec.describe Article, type: :model do
 
     context "title" do
       it 'titleが空の場合、不正データとなる' do
-        
+
         article.title = ''
- 
+
         expect(article).not_to be_valid
         expect(article.errors[:title]).to include("can't be blank")
+      end
+
+      it 'titleが一意でない場合、不正データとなる' do
+        article_1 = create(:article)
+        expect(article_1).to be_valid
+
+        article_2 = build(:article, title: article_1.title)
+
+        expect(article_2).not_to be_valid
+        expect(article_2.errors[:title]).to include("has already been taken")
+
+        article_2.title = 'new title'
+        expect(article_2).to be_valid
       end
     end
   end
