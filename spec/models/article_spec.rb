87c9a0679514cb948a99046819017c98495c2a67
Ryuya Ishibashi <vincitori.o.morti@gmail.com>
2023-01-21 07:56:33 +0900

    model 正常系test

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:article) { build(:article) }
+
+  describe "#validations" do
+    it '正常なデータが作成できること' do
+      expect(article).to be_valid
+    end
+  end
 end
