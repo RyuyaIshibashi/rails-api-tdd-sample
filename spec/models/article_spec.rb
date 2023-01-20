d843526aea376d8a27a957cc139a6a982046bb68
Ryuya Ishibashi <vincitori.o.morti@gmail.com>
2023-01-21 08:07:23 +0900

    add title require test

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -7,5 +7,15 @@ RSpec.describe Article, type: :model do
     it '正常なデータが作成できること' do
       expect(article).to be_valid
     end
+
+    context "title" do
+      it 'titleが空の場合、不正データとなる' do
+        
+        article.title = ''
+ 
+        expect(article).not_to be_valid
+        expect(article.errors[:title]).to include("can't be blank")
+      end
+    end
   end
 end
